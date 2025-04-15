71d50ac58f71b4fc56331954ec9d6b4e86d84dc7
Karthik <karthikkutty126@gmail.com>
2025-04-16 00:21:48 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path';
 
 export default defineConfig({
   plugins: [vue()],
   server: {
     port: 3000
   },
-  base: '/qr-generator',
-  build: {
-    outDir: 'dist'
-  },
-  resolve: {
-    alias: {
-      '@modules': path.resolve(__dirname, './modules'),
-    },
-},
+
 }) 
